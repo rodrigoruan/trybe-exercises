2aca2ca3a2f50007ef7582925167af81359dc2d5
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-03-24 22:13:10 -0300

    Refatorando exercicios 11

--- exercises/bloco_4/dia_1/exercicio11.js
+++ exercises/bloco_4/dia_1/exercicio11.js
@@ -1,31 +1,35 @@
-let salarioBruto = 3000;
+let salario = 3000;
 
 // Desconto INSS
 
-if (salarioBruto >= 0 && salarioBruto <= 1556.94) {
-  salarioBruto = salarioBruto - (salarioBruto / 100) * 8;
-} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-  salarioBruto = salarioBruto - (salarioBruto / 100) * 9;
-} else if (salarioBruto >= 2593.93 && salarioBruto <= 5189.82) {
-  salarioBruto = salarioBruto - (salarioBruto / 100) * 11;
+if (salario <= 1556.94) {
+  salario = salario - (salario / 100) * 8;
+} else if (salario >= 1556.95 && salario <= 2594.92) {
+  salario = salario - (salario / 100) * 9;
+} else if (salario >= 2593.93 && salario <= 5189.82) {
+  salario = salario - (salario / 100) * 11;
 } else {
-  salarioBruto - 570.88;
+  salario - 570.88;
 }
 
 // Desconto Imposto de Renda IR
 
-if (salarioBruto <= 1903.98) {
-  console.log(`Seu salario bruto é de ${salarioBruto}`);
-} else if (salarioBruto >= 1903.99 && salarioBruto <= 2826.65) {
-  salarioBruto = salarioBruto - ((salarioBruto / 100) * 7.5 - 142.8);
-  console.log(`Seu salario bruto é de ${salarioBruto}`);
-} else if (salarioBruto >= 2826.66 && salarioBruto <= 3751.05) {
-  salarioBruto = salarioBruto - ((salarioBruto / 100) * 15 - 354.8);
-  console.log(`Seu salario bruto é de ${salarioBruto}`);
-} else if (salarioBruto >= 3751.06 && salarioBruto <= 4664.68) {
-  salarioBruto = salarioBruto - ((salarioBruto / 100) * 22.5 - 636.13);
-  console.log(`Seu salario bruto é de ${salarioBruto}`);
+if (salario <= 1903.98) {
+  salario;
+} else if (salario >= 1903.99 && salario <= 2826.65) {
+  salario = salario - ((salario / 100) * 7.5 - 142.8);
+} else if (salario >= 2826.66 && salario <= 3751.05) {
+  salario = salario - ((salario / 100) * 15 - 354.8);
+} else if (salario >= 3751.06 && salario <= 4664.68) {
+  salario = salario - ((salario / 100) * 22.5 - 636.13);
 } else {
-  salarioBruto = salarioBruto - ((salarioBruto / 100) * 27.5 - 869.36);
-  console.log(`Seu salario bruto é de ${salarioBruto}`);
+  salario = salario - ((salario / 100) * 27.5 - 869.36);
+}
+
+// CONSOLE LOG
+
+if (salario > 0) {
+  console.log(`Seu salario bruto é de ${salario}`);
+} else {
+  console.log(`Digite um salário válido`);
 }
