6bc516aa3c64ca96dc92d451ceba21632cb53cd9
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-03-26 20:25:25 -0300

    Adicionado exercicio 2

---
+++ exercises/bloco_4/dia_3/exercicio1.js
@@ -0,0 +1,6 @@
+let valor = 5; //Declarei uma variavel para guardar o valor dado
+
+for (let i = 0; i < valor; i += 1) {
+  // criado uma repetição para repetir enquanto o indíce for menor que o valor da variavel, continuara repetindo até chegar no valor da variável
+  console.log('*'.repeat(valor)); //criado um console para imprimir os astericos * o valor da variavel ex: se o valor for cinco imprimirá assim '*****' no console
+}
