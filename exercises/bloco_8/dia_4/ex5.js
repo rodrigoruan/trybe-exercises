4e308b7b996c365aae585e054e3acb548b3f9d16
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-05 09:05:25 -0300

    Adicionado exercicio 5

---
+++ exercises/bloco_8/dia_4/ex5.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',
+  'Adervandes',
+  'Akifusa',
+  'Abegildo',
+  'Adicellia',
+  'Aladonata',
+  'Abeladerco',
+  'Adieidy',
+  'Alarucha',
+];
+
+const containsA = () => names.join('').replace(/[^A]/gi, '').length;
+
+assert.deepStrictEqual(containsA(), 20);
