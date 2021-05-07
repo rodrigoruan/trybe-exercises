af7c7802e46cf1e4caee3ae7405affbd45347f80
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-07 10:39:36 -0300

    Adicionado exercicio 3

--- exercises/bloco_8/dia_4/ex1.js
+++ exercises/bloco_8/dia_4/ex1.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
 
-const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr, 0);
+const rectangleArea = (width, height) => width * height;
 
-assert.strictEqual(sum(), 0);
-assert.strictEqual(sum(1), 1);
-assert.strictEqual(sum(1, 2), 3);
-assert.strictEqual(sum(1, 2, 3), 6);
-assert.strictEqual(sum(1, 2, 3, 4), 10);
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((...rectangle) => {
+  assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
+});
