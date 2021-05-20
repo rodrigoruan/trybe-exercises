7d2daf60fe7986726baf9369a7a610b8b0cb8842
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-20 18:57:31 -0300

    Adicionado exercicio 3

---
+++ exercises/bloco_10/dia_3/ex3/ex3.test.js
@@ -0,0 +1,16 @@
+const randomNumber = require('./ex3');
+jest.mock('./ex3');
+
+test('multiplicação', () => {
+  randomNumber.mockImplementation((a, b, c) => a * b * c);
+  expect(randomNumber(2, 2, 2)).toBe(8);
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toHaveBeenCalledWith(2, 2, 2);
+
+  randomNumber.mockRestore();
+
+  randomNumber.mockImplementation((value) => value * 2);
+  expect(randomNumber(2)).toBe(4);
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toHaveBeenCalledWith(2);
+});
