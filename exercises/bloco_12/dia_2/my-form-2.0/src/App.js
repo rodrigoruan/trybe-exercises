990672d91c30f398d7b4a3c947b3e24e4fe96ae7
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-29 19:42:32 -0300

    Adiciona requisito Bonus

--- exercises/bloco_12/dia_2/my-form-2.0/src/App.js
+++ exercises/bloco_12/dia_2/my-form-2.0/src/App.js
@@ -9,6 +9,7 @@ export class App extends Component {
     this.state = {
       nome: '',
     };
+
     this.showInfos = this.showInfos.bind(this);
     this.onBlur = this.onBlur.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -16,36 +17,29 @@ export class App extends Component {
 
   handleClick({ target }) {
     const { name, value } = target;
+
     this.setState({
-      [name]:
-        name === 'nome'
-          ? value.toUpperCase()
-          : name === 'endereco'
-          ? value.replace(/\W/g, '')
-          : value,
+      [name]: name === 'nome' ? value.toUpperCase() 
+      : name === 'endereco' ? value.replace(/\W/g, '')
+      : value,
     });
   }
 
   showInfos() {
     this.setState({
       form: (
-        <div>
-          {Object.entries(this.state).map((elemento) => (
-            <p key={elemento[0]}>
-              {elemento[0]}: {elemento[1]}
-            </p>
-          ))}
-        </div>
-      ),
+      <div> 
+        {Object.entries(this.state).map((elemento) => ( 
+        <p key={elemento[0]}>
+          {elemento[0]}: {elemento[1]}
+        </p>))}
+      </div>),
     });
   }
 
   onBlur({ target }) {
     const { name, value } = target;
-    if (/^\d/.test(value))
-      this.setState({
-        [name]: '',
-      });
+    if (/^\d/.test(value)) this.setState({ [name]: '', });
   }
 
   render() {
@@ -53,117 +47,64 @@ export class App extends Component {
     return (
       <form>
         <fieldset>
-          <Input
-            maxLength='40'
-            value={this.state.nome}
-            required
-            name='nome'
-            onChange={this.handleClick}
-          />
-          <Input
-            value={this.state.email}
-            name='email'
-            maxLength='50'
-            required
-            onChange={this.handleClick}
-          />
-          <Input
-            name='cpf'
-            value={this.state.cpf}
-            onChange={this.handleClick}
-            required
-            maxLength='11'
-          />
-          <Input
-            name='endereco'
-            value={this.state.endereco}
-            onChange={this.handleClick}
-            required
-            maxLength='200'
-          />
-          <Input
-            name='cidade'
-            onBlur={this.onBlur}
-            value={this.state.cidade}
-            onChange={this.handleClick}
-            required
-            maxLength='28'
-          />
+          <Input maxLength='40' value={this.state.nome} placeHolder='Nome'
+            required name='nome' onChange={this.handleClick}/>
+
+          <Input value={this.state.email} name='email' placeHolder='Email'
+            maxLength='50' required onChange={this.handleClick}/>
+
+          <Input name='cpf' placeHolder='Cpf' value={this.state.cpf}
+            onChange={this.handleClick} required maxLength='11'/>
+
+          <Input name='endereco' placeHolder='Endereço' value={this.state.endereco}
+            onChange={this.handleClick} required maxLength='200'/>
+
+          <Input name='cidade' placeHolder='Cidade' onBlur={this.onBlur}
+            value={this.state.cidade} onChange={this.handleClick} required maxLength='28'/>
+
           <select name='estados' required onChange={this.handleChange}>
-            <option>Selecione algum estado</option>
-            {estados.map((item, index) => (
-              <option key={index}>{item}</option>
-            ))}
+            {estados.map((item, index) => (<option key={index}>{item}</option>))}
           </select>
-          <Input
-            onChange={this.handleClick}
-            required
-            value='casa'
-            name='house'
-            type='radio'
-          />
-          <Input
-            onChange={this.handleClick}
-            required
-            value='apto'
-            name='house'
-            type='radio'
-          />
+
+          <Input title='casa' onChange={this.handleClick} required 
+            value='casa' name='house' type='radio'/>
+
+          <Input onChange={this.handleClick} required title='apto'
+            value='apto' name='house' type='radio'/>
         </fieldset>
+
         <fieldset>
-          <textarea
-            value={this.state.resumo}
-            name='resumo'
-            onChange={this.handleClick}
-            maxLength='1000'
-            required
-          />
-          <textarea
-            value={this.state.cargo}
-            name='cargo'
+          <textarea value={this.state.resumo} placeHolder='Resumo' name='resumo'
+            onChange={this.handleClick} maxLength='1000' required/>
+    
+          <textarea value={this.state.cargo} placeHolder='Cargo' name='cargo'
+            onChange={this.handleClick} required maxLength='40'
             onMouseEnter={() => {
               if (active) {
                 alert('Exiba essa mensagem apenas uma vez.');
                 active = false;
-              }
-            }}
-            onChange={this.handleClick}
-            required
-            maxLength='40'
-          />
-          <textarea
-            value={this.state.description}
-            name='description'
-            onChange={this.handleClick}
-            required
-            maxLength='500'
-          />
+              }}} />
+
+          <textarea value={this.state.description} placeHolder='Descrição'
+            name='description' onChange={this.handleClick} required maxLength='500'/>
         </fieldset>
-        <button
-          onClick={(e) => {
-            this.showInfos();
+
+        <button onClick={(e) => {
+            if (!/\w+@\w+\.com(\.br)?/.test(this.state.email)) {
+              alert('Digite um email válido');
+            } else {
+              this.showInfos();
+            }
             e.preventDefault();
-          }}>
-          Mostrar informações
-        </button>
-        <button
-          onClick={(e) => {
-            this.setState({
-              cargo: '',
-              cidade: '',
-              cpf: '',
-              description: '',
-              email: '',
-              endereco: '',
-              form: '',
-              house: '',
-              nome: '',
-              resumo: '',
+          }}> Mostrar informações </button>
+
+        <button onClick={(e) => {this.setState({ cargo: '',
+              cidade: '', cpf: '', description: '', email: '',
+              endereco: '', form: '', house: '', nome: '', resumo: '',
             });
             e.preventDefault();
-          }}>
-          Limpar
-        </button>
+          }}> Limpar </button>
+
         {this.state.form}
       </form>
     );
