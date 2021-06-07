93bd01a35ebb896a057fed6e755034e292b3c0f0
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-07 11:37:09 -0300

    Adiciona requisito 10 - fixacao

--- exercises/bloco_13/dia_2/comprehension-exercises-router-master/App.js
+++ exercises/bloco_13/dia_2/comprehension-exercises-router-master/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
 
@@ -8,17 +8,16 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/users'>Users</Link>
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route
-          path='/users/:id'
-          render={(props) => (
-            <Users {...props} greetingMessage='Good Morning' />
-          )}
-        />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route
+            path='/users/:id'
+            render={(props) => (
+              <Users {...props} greetingMessage='Good Morning' />
+            )}
+          />
+        </Switch>
       </BrowserRouter>
     );
   }
