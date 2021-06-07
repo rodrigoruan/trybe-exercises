3ca703c71a43d3e63e9b4e4f161944f0d0e39499
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-07 11:41:15 -0300

    Adiciona requisito 11 - fixacao

---
+++ exercises/bloco_13/dia_2/comprehension-exercises-router-master/StrictAcess.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+
+const StrictAcess = ({ user: { username, password } }) => {
+  if (/joao/.test(username) && /1234/.test(password)) {
+    return <p>Welcome joao!</p>;
+  }
+  alert('Acess denied');
+  return <Redirect to='/' />;
+};
+
+export default StrictAcess;
