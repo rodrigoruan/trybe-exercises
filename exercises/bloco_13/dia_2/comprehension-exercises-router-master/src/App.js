e84f7f852dd8564e3113abeadca9b8b03aad0af2
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-07 11:02:27 -0300

    Adiciona requisito 6 - fixacao

--- exercises/bloco_13/dia_2/comprehension-exercises-router-master/src/App.js
+++ exercises/bloco_13/dia_2/comprehension-exercises-router-master/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
 
@@ -8,6 +8,9 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to='/' >Home</Link>
+        <Link to='/about' >About</Link>
+        <Link to='/users' >Users</Link>
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/users' component={Users} />
