6c88ab058ab31d5c0c2b586457e6eea14be8a163
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-07 10:56:18 -0300

    Adiciona requisito 1 - fixacao

---
+++ exercises/bloco_13/dia_2/comprehension-exercises-router-master/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
