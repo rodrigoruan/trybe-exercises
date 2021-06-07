646ca2c898218eee5f1c8ab8d21af45055f0e6e3
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-07 11:42:18 -0300

    Adiciona requisito 12 - fixacao

--- exercises/bloco_13/dia_2/comprehension-exercises-router-master/App.js
+++ exercises/bloco_13/dia_2/comprehension-exercises-router-master/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link path='/strict-access'>Strict Access</Link>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
