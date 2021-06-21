5c448142f9e8808f038bd963175d6533b0dbc60a
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-21 18:12:31 -0300

    Requeriment 2 Added

---
+++ exercises/bloco_15/exercise-todo-list-master/src/test/Requeriment2.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import Item from '../Item';
+
+describe('Testando a aplicação, testando o botão de adicionar tarefa', () => {
+  const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água'];
+  const { getByTestId, getByLabelText, getAllByTestId } = render(<App />);
+  const sendButton = getByTestId('id-send-button');
+  const inputTask = getByLabelText('Tarefa:');
+
+  listTodo.forEach((task) => {
+    fireEvent.change(inputTask, { target: { value: task } });
+    fireEvent.click(sendButton);
+  });
+
+  const allTasks = getAllByTestId('id-task-saved');
+
+  it('Verificando se as tarefas foram adicionadas ao digitar elas e clicar no botão', () => {
+    expect(allTasks.length).toBe(3);
+    allTasks.forEach((task) => {
+      expect(task).toBeInTheDocument();
+    });
+  });
+});
+
+describe('Testando a aplicação, testando o botão de adicionar tarefa', () => {
+  const { getByText } = render(<Item content='Comer tapioca' />);
+  const inputValue = getByText('Comer tapioca');
+
+  it('Verifica se ao passar um texto para o componente Item, o mesmo é renderizado', () => {
+    expect(inputValue).toBeTruthy();
+  });
+});
