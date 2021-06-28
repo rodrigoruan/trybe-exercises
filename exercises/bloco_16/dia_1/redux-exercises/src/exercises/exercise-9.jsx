2c80572e903ecc7e19e72d7a4cb322f7cecb0d31
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-28 16:28:10 -0300

    Added requeriment 10

--- exercises/bloco_16/dia_1/redux-exercises/src/exercises/exercise-9.jsx
+++ exercises/bloco_16/dia_1/redux-exercises/src/exercises/exercise-9.jsx
@@ -7,9 +7,9 @@ const DECREMENT = 'DECREMENT' // Define a constant for decrement action types
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-      return (state += 1)
+      return state + 1
     case DECREMENT:
-      return (state -= 1)
+      return state - 1
     default:
       return state
   }
