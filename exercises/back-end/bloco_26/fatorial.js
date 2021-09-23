cbca39299f0fccd8330487e506ba09251b662cef
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 13:15:08 -0300

    Added script to fatorial

--- exercises/back-end/bloco_26/fatorial.js
+++ exercises/back-end/bloco_26/fatorial.js
@@ -1,1 +1,19 @@
 const readline = require("readline-sync");
+
+function fatorial() {
+  const num = readline.questionInt("Digite um número maior que 0: ");
+
+  function fatorial(n) {
+    if (n <= 0) return "Número inválido";
+
+    return Array.from(Array(n), (_, i) => i + 1).reduce(
+      (acc, curr) => acc * curr
+    );
+  }
+
+  console.log(fatorial(num));
+}
+
+fatorial();
+
+module.exports = fatorial;
--- exercises/back-end/bloco_26/index.js
+++ exercises/back-end/bloco_26/index.js
@@ -3,12 +3,14 @@ const readline = require("readline-sync");
 console.log(`Scripts disponíveis:
 1 - Velocidade
 2 - Sorteio
-3 - IMC`);
+3 - IMC
+4 - Fatorial`);
 
 const response = readline.questionInt(
-  "Qual script você deseja rodar? (1/2/3) "
+  "Qual script você deseja rodar? (1/2/3/4) "
 );
 
 if (response === 1) require("./velocidade")();
 else if (response === 2) require("./sorteio")();
 else if (response === 3) require("./imc")();
+else if (response === 4) require('./fatorial')
