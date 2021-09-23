d8a5e6f7b5e0e1bad670aa7c60e08520e0f5d2ce
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 12:17:52 -0300

    Added final answer to play again or quit the game

--- exercises/back-end/bloco_26/sorteio.js
+++ exercises/back-end/bloco_26/sorteio.js
@@ -1,19 +1,28 @@
 const readline = require("readline-sync");
 
-function generateRandomNum() {
-  return Math.round(Math.random() * 10);
-}
-
-const response = readline.questionInt("Digite um número de 0 a 10: ");
-
-function verifyAnswer() {
-  const num = generateRandomNum();
-
-  if (response === num) {
-    return "Parabéns, número correto!";
-  } else {
-    return `Opa, não foi dessa vez. O número era ${num}`;
+function game() {
+  function generateRandomNum() {
+    return Math.round(Math.random() * 10);
   }
+
+  const response = readline.questionInt("Digite um número de 0 a 10: ");
+
+  function verifyAnswer() {
+    const num = generateRandomNum();
+
+    if (response === num) {
+      return "Parabéns, número correto!";
+    } else {
+      return `Opa, não foi dessa vez. O número era ${num}`;
+    }
+  }
+
+  console.log(verifyAnswer(response));
+
+  const playAgain = readline.question("Deseja jogar novamente? (y/n)");
+
+  if (playAgain === "y") game();
+  else console.log("Até mais!");
 }
 
-console.log(verifyAnswer(response));
+game();
