54cfc8d075909d08d3f80d18f7cba38d0b64d17f
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 11:53:43 -0300

    Done script velocidade

--- exercises/back-end/bloco_26/velocidade.js
+++ exercises/back-end/bloco_26/velocidade.js
@@ -1,1 +1,6 @@
-console.log()
+const readline = require("readline-sync");
+
+const distancia = readline.questionInt("Digite a distância: ");
+const tempo = readline.questionInt("Digite o tempo: ");
+
+console.log(`A velocidade é de ${distancia / tempo}`);
