d2ef2d375d9b61aa20dc5f90447d35a94c387b2c
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 11:56:29 -0300

    Added question with readline

--- exercises/back-end/bloco_26/sorteio.js
+++ exercises/back-end/bloco_26/sorteio.js
@@ -1,1 +1,5 @@
+const readline = require("readline-sync");
+
 const generateRandomNum = () => Math.round(Math.random() * 10);
+
+const response = readline.questionInt("Digite um número de 0 a 10: ");
