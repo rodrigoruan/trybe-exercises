fae4603ae16da96be1b47605368972d107ee2474
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 11:49:33 -0300

    Added IMCTable function to show a message with stage of IMC

--- exercises/back-end/bloco_26/imc.js
+++ exercises/back-end/bloco_26/imc.js
@@ -3,4 +3,16 @@ const readline = require("readline-sync");
 const peso = readline.questionFloat("Digite seu peso: ");
 const altura = readline.questionFloat("Digite sua altura: ");
 
-console.log(`Seu IMC é de ${peso / altura ** 2}`);
+const IMCTable = (IMC) => {
+  if(IMC < 18.5) return "Abaixo do peso"
+  else if(IMC < 24.9) return "Peso normal"
+  else if(IMC < 29.9) return "Acima do peso"
+  else if(IMC < 35.9) return "Obesidade grau 1"
+  else if(IMC < 39.9) return "Obesidade grau 2"
+  else return "Obesidade grau 3"
+};
+
+const IMC = peso / altura ** 2;
+
+console.log(`Seu IMC é de ${IMC}`);
+console.log(IMCTable(IMC));
