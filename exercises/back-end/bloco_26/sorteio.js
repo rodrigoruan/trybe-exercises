490ae2675a62817199e1ed692a0dcc077c22f54e
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 12:00:36 -0300

    Added function to verify answer

--- exercises/back-end/bloco_26/sorteio.js
+++ exercises/back-end/bloco_26/sorteio.js
@@ -1,5 +1,19 @@
 const readline = require("readline-sync");
 
-const generateRandomNum = () => Math.round(Math.random() * 10);
+function generateRandomNum() {
+  return Math.round(Math.random() * 10);
+}
 
 const response = readline.questionInt("Digite um número de 0 a 10: ");
+
+function verifyAnswer() {
+  const num = generateRandomNum();
+
+  if (response === num) {
+    return "Parabéns, número correto!";
+  } else {
+    return `Opa, não foi dessa vez. O número era ${num}`;
+  }
+}
+
+console.log(verifyAnswer(response));
