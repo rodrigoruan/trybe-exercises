0b4e8ef3fd76c37d306e896d007a07d6c10dada7
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 13:43:18 -0300

    Done exercises

--- exercises/back-end/bloco_26/fibonacci.js
+++ exercises/back-end/bloco_26/fibonacci.js
@@ -1,7 +1,16 @@
 const filreader = require("readline-sync");
 
 function fibonacci() {
-  const answer = filreader.questionInt("Digite um número maior que 0: ");
+  const r = filreader.questionInt("Digite um número maior que 0: ");
+  const arr = [];
 
-  
+  arr[0] = 0;
+  arr[1] = 1;
+
+  for (let i = 2; i <= r; i += 1) {
+    arr[i] = arr[i - 2] + arr[i - 1];
+    console.log(arr[i]);
+  }
 }
+
+fibonacci();
