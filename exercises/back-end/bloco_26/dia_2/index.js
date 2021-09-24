e42153e349f67a39cde3165cd1d755667f1b789d
rodrigoruan <rodrigopython16@gmail.com>
2021-09-24 16:53:45 -0300

    Added ex.1 bonus

--- exercises/back-end/bloco_26/dia_2/index.js
+++ exercises/back-end/bloco_26/dia_2/index.js
@@ -1,13 +1,29 @@
 const fs = require("fs").promises;
 const readline = require("readline-sync");
 
-const script = readline.questionInt("Qual script você deseja rodar? (1...5)");
+function showContent() {
+  const script = readline.questionInt("Qual script você deseja rodar? (1...5)");
 
-function verifyScript() {
-  return fs
-    .readFile(`file${script}.txt`, "utf8")
-    .then((r) => console.log(r))
-    .catch((err) => console.log("Arquivo inexistente"));
+  function verifyScript() {
+    return fs
+      .readFile(`file${script}.txt`, "utf8")
+      .then((r) => console.log(r))
+      .catch((err) => console.log("Arquivo inexistente"));
+  }
+
+  verifyScript();
 }
 
-verifyScript();
+function writeContet() {
+  const script = readline.questionInt(
+    "Qual arquivo você deseja alterar? (1...5)"
+  );
+}
+
+const s = readline.questionInt(`Qual função vc deseja rodar ? 
+1 - readContent
+2 - writeFile`);
+
+if (s === 1) showContent()
+else if (s === 2) writeContet()
+else console.log("Função inexistente")
