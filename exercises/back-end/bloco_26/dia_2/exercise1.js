08d189d4cd2154eed6b85240502e836cc7d0f6ec
rodrigoruan <rodrigopython16@gmail.com>
2021-09-24 15:33:42 -0300

    Added exercise 4

--- exercises/back-end/bloco_26/dia_2/exercise1.js
+++ exercises/back-end/bloco_26/dia_2/exercise1.js
@@ -1,36 +1,50 @@
+const fs = require("fs").promises;
+
 // 1
 
-function func(a, b, c) {
-  return new Promise((resolve, reject) => {
-    if ([a, b, c].some((num) => typeof num !== "number")) {
-      reject(Error("Informe apenas números"));
-    }
+// function func(a, b, c) {
+//   return new Promise((resolve, reject) => {
+//     if ([a, b, c].some((num) => typeof num !== "number")) {
+//       reject(Error("Informe apenas números"));
+//     }
 
-    const total = (a + b) * c;
+//     const total = (a + b) * c;
 
-    if (total < 50) reject(Error("Valor muito baixo"));
-    resolve(total);
-  });
-}
+//     if (total < 50) reject(Error("Valor muito baixo"));
+//     resolve(total);
+//   });
+// }
 
 // 2
 
-function randomNum() {
-  return Math.floor(Math.random() * 100 + 1);
-}
+// function randomNum() {
+//   return Math.floor(Math.random() * 100 + 1);
+// }
 
 // func(randomNum(), randomNum(), randomNum())
 //   .then((r) => console.log(r))
 //   .catch((err) => console.log(err.message));
 
 // 3
-async function resolvePromise() {
-  try {
-    const r = await func(randomNum(), randomNum(), randomNum());
-    console.log(r);
-  } catch (err) {
-    console.log(err.message);
-  }
+// async function resolvePromise() {
+//   try {
+//     const r = await func(randomNum(), randomNum(), randomNum());
+//     console.log(r);
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// }
+
+// resolvePromise();
+
+//4
+function showAllCharacters() {
+  fs.readFile("simpsons.json", "utf8")
+    .then((response) => {
+      const parsedArray = JSON.parse(response);
+      parsedArray.forEach(({ id, name }) => console.log(`${id} - ${name}`));
+    })
+    .catch((err) => console.error(`Erro ao ler o arquivo: ${err}`));
 }
 
-resolvePromise();
+showAllCharacters();
