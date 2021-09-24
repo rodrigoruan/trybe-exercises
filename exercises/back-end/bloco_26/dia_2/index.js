938a90c9500194748dae7e56f2a1888926d96c9f
rodrigoruan <rodrigopython16@gmail.com>
2021-09-24 17:00:55 -0300

    added all bonus exercises

--- exercises/back-end/bloco_26/dia_2/index.js
+++ exercises/back-end/bloco_26/dia_2/index.js
@@ -18,12 +18,26 @@ function writeContet() {
   const script = readline.questionInt(
     "Qual arquivo você deseja alterar? (1...5)"
   );
+  fs.readFile(`file${script}.txt`, "utf8")
+    .then((r) => {
+      const word = readline.question("Qual palavra deseja alterar?");
+      const newWord = readline.question(
+        "Qual palavra deseja inserir no lugar?"
+      );
+      const rgx = new RegExp(word, "g");
+      const newSentence = r.replace(rgx, newWord);
+      console.log(newSentence);
+      const path = readline.question("Qual caminho de destino?");
+      fs.writeFile(path, newSentence);
+    })
+    .catch((err) => console.log("Arquivo inexistente"));
 }
 
 const s = readline.questionInt(`Qual função vc deseja rodar ? 
 1 - readContent
-2 - writeFile`);
+2 - writeFile
+R: `);
 
-if (s === 1) showContent()
-else if (s === 2) writeContet()
-else console.log("Função inexistente")
+if (s === 1) showContent();
+else if (s === 2) writeContet();
+else console.log("Função inexistente");
