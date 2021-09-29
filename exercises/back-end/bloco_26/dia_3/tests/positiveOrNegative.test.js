01b32dc8ecca94a77c6cf3c0a81d05efae47e87e
rodrigoruan <rodrigopython16@gmail.com>
2021-09-29 15:28:16 -0300

    Validando se as respostas sao do tipo string

--- exercises/back-end/bloco_26/dia_3/tests/positiveOrNegative.test.js
+++ exercises/back-end/bloco_26/dia_3/tests/positiveOrNegative.test.js
@@ -5,19 +5,21 @@ describe("Testing positive or negative function", () => {
   it('When passed a that number is over than 0, should return "positivo"', () => {
     const response = positiveOrNegative(2);
 
-    expect(reponse).to.be.a("string");
+    expect(response).to.be.a("string");
     expect(response).to.be.equal("positivo");
   });
 
   it('When passed a number that is lower than 0, should return "negativo"', () => {
     const response = positiveOrNegative(-1);
 
+    expect(response).to.be.a("string");
     expect(response).to.be.equal("negativo");
   });
 
   it('When passed a number that is 0, should return "neutro"', () => {
     const response = positiveOrNegative(0);
 
+    expect(response).to.be.a("string");
     expect(response).to.be.equal("neutro");
   });
 });
