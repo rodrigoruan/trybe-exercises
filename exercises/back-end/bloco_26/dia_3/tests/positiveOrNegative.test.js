ed3b664e3c34fe63d579d703e0efd8473e3e9efc
rodrigoruan <rodrigopython16@gmail.com>
2021-09-29 15:24:15 -0300

    Added exercise 1

--- exercises/back-end/bloco_26/dia_3/tests/positiveOrNegative.test.js
+++ exercises/back-end/bloco_26/dia_3/tests/positiveOrNegative.test.js
@@ -2,9 +2,16 @@ const { expect } = require("chai");
 const positiveOrNegative = require("../functions/positiveOrNegative");
 
 describe("Testing positive or negative function", () => {
-  it('When the passed number is over than 0, should return "positivo"', () => {
+  it('When passed a that number is over than 0, should return "positivo"', () => {
     const response = positiveOrNegative(2);
 
     expect(response).to.be.equal("positivo");
   });
+
+  it('When passed a number that is lower than 0, should return "negativo"'),
+    () => {
+      const response = positiveOrNegative(-1);
+
+      expect(response).to.be.equal("negativo");
+    };
 });
