2dbe4ee6ffa86bb6e4d7be9d01cd917d4fa25fb8
rodrigoruan <rodrigopython16@gmail.com>
2021-09-30 12:03:03 -0300

    Added exercise 3

--- exercises/back-end/bloco_26/dia_4/hello-express/index.js
+++ exercises/back-end/bloco_26/dia_4/hello-express/index.js
@@ -127,3 +127,13 @@ app.post("/hello", (req, res) => {
 
   return res.status(200).json({ message: `Hello, ${name}!` });
 });
+
+app.post("/greetings", (req, res) => {
+  const { name, age } = req.body;
+
+  if (age > 17) {
+    return res.status(200).json({ message: `Hello, ${name}!` });
+  }
+
+  return res.status(401).json({ message: `Unauthorized` });
+});
