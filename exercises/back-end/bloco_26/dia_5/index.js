a4294cc9d8fd346674a53da7faa0c284546ebfc0
rodrigoruan <rodrigopython16@gmail.com>
2021-10-04 20:09:39 -0300

    Added 4.1

--- exercises/back-end/bloco_26/dia_5/index.js
+++ exercises/back-end/bloco_26/dia_5/index.js
@@ -11,17 +11,31 @@ app.use(bodyParser.json());
 
 // app.use("/user", userRouter);
 
-app.get("/btc", async (req, res) => {
-  const token = req.headers.authorization;
+// app.get("/btc", async (req, res) => {
+//   const token = req.headers.authorization;
 
-  if (token.length !== 12 || /^[^A-Za-z\d]{12}$/.test(token)) {
-    return res.status(400).json({ message: "invalid token" });
+//   if (token.length !== 12 || /^[^A-Za-z\d]{12}$/.test(token)) {
+//     return res.status(400).json({ message: "invalid token" });
+//   }
+
+//   const json = await axios.get(
+//     "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+//   );
+//   res.status(200).json(json.data);
+// });
+
+const posts = [{ id: 1, name: "HELLO" }];
+
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  const filteredPost = posts.find((post) => post.id === Number(id));
+
+  if (!filteredPost) {
+    return res.status(400).json({ message: "post not found" });
   }
 
-  const json = await axios.get(
-    "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
-  );
-  res.status(200).json(json.data);
+  res.status(200).json({ filteredPost });
 });
 
 app.listen(3001, () => {
