4a641ec22d3f8bc007a423828c00dfccebf1731c
rodrigoruan <rodrigopython16@gmail.com>
2021-10-04 19:01:33 -0300

    Added exercise 1

--- exercises/back-end/bloco_26/dia_5/index.js
+++ exercises/back-end/bloco_26/dia_5/index.js
@@ -1,79 +1,25 @@
+/* index.js */
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 
-const recipes = [
-  { id: 1, name: "Lasanha", preco: 40.0, tempoDePreparo: 30 },
-  { id: 2, name: "Macarrão a Bolonhesa", preco: 35.0, tempoDePreparo: 25 },
-  { id: 3, name: "Macarrão com molho branco", preco: 35.0, tempoDePreparo: 25 },
-];
+app.post("/user/register", (req, res) => {
+  const { username, email, password } = req.body;
 
-function validatePrice(req, res, next) {
-  const { preco } = req.body;
-
-  if (!preco || preco < 0) {
-    return res.status(400).json({ message: "Preco invalido" });
+  if (
+    username.length <= 3 ||
+    !/\w+@\w+.com/.test(email) ||
+    password.length < 4 ||
+    password.length > 8
+  ) {
+    res.status(400).json({ message: "invalid data" });
   }
-  next();
-}
 
-app.get("/recipes", function (_req, res) {
-  res.status(200).json(recipes);
-});
-
-app.get("/recipes/pesquisar", function (req, res) {
-  const { name, maxPrice } = req.query;
-  const filteredRecipes = recipes.filter(
-    (r) => r.name.includes(name) && r.preco < parseInt(maxPrice)
-  );
-  res.status(200).json(filteredRecipes);
-});
-
-app.get("/recipes/:id", function (req, res) {
-  const { id } = req.params;
-  const recipe = recipes.find((r) => r.id === parseInt(id));
-  if (!recipe) return res.status(404).json({ message: "Recipe not found!" });
-
-  res.status(200).json(recipe);
-});
-
-app.post("/recipes", validatePrice, function (req, res) {
-  const { id, name, price } = req.body;
-  recipes.push({ id, name, price });
-  res.status(201).json({ message: "Recipe created successfully!" });
-});
-
-app.put("/recipes/:id", validatePrice, function (req, res) {
-  const { id } = req.params;
-  const { name, price } = req.body;
-  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
-
-  if (recipeIndex === -1)
-    return res.status(404).json({ message: "Recipe not found!" });
-
-  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
-
-  res.status(204).end();
-});
-
-app.delete("/recipes/:id", function (req, res) {
-  const { id } = req.params;
-  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
-
-  if (recipeIndex === -1)
-    return res.status(404).json({ message: "Recipe not found!" });
-
-  recipes.splice(recipeIndex, 1);
-
-  res.status(204).end();
-});
-
-app.all("*", function (req, res) {
-  return res.status(404).json({ message: `Rota '${req.path}' não existe!` });
+  res.status(200).json({ message: "user created" });
 });
 
 app.listen(3001, () => {
-  console.log("Aplicação ouvindo na porta 3001");
+  console.log("Ouvindo na porta 3001");
 });
