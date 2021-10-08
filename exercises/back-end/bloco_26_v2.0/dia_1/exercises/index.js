03fe58b6fa7c614917bae0eb50451591763585aa
rodrigoruan <rodrigopython16@gmail.com>
2021-10-08 13:17:03 -0300

    REFATORED EX_1

--- exercises/back-end/bloco_26_v2.0/dia_1/exercises/index.js
+++ exercises/back-end/bloco_26_v2.0/dia_1/exercises/index.js
@@ -1,19 +1,30 @@
 const express = require('express');
 
-const app = express();
-
 const User = require('./model/User');
+
+const app = express();
 
 app.use(express.json());
 
-app.post('/user', (req, res) => {
+app.get('/user', async (_req, res) => {
+  const response = await User.getAllUsers();
+
+  res.status(200).json(response);
+});
+
+app.post('/user', async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  const response = User.createNewUser(firstName, lastName, email, password);
+  const response = await User.createNewUser(
+    firstName,
+    lastName,
+    email,
+    password
+  );
 
   if (!response) return res.status(400).json({ message: 'Dados inválidos' });
 
-  res.status(200).json({ message: 'Usuario criado com sucesso' });
+  res.status(200).json(response);
 });
 
 app.listen(3000, () => {
--- exercises/back-end/bloco_26_v2.0/dia_1/exercises/model/User.js
+++ exercises/back-end/bloco_26_v2.0/dia_1/exercises/model/User.js
@@ -1,11 +1,20 @@
-const connection = require('./connection');
+const connection = require('./connections');
 
-function createNewUser(firstName, lastName, email, password) {
-  if ([firstName, lastName, email, password].includes(undefined)) return false;
-
-  return connection().then((db) =>
-    db.collection('users').insertOne({ firstName, lastName, email, password })
-  );
+async function getAllUsers() {
+  return connection()
+    .then((db) => db.collection('users').find().toArray())
+    .then((r) => r);
 }
 
-module.exports = { createNewUser };
+async function createNewUser(firstName, lastName, email, password) {
+  if ([firstName, lastName, email, password].includes(undefined)) return false;
+  if (password.length <= 6) return false;
+
+  return connection()
+    .then((db) =>
+      db.collection('users').insertOne({ firstName, lastName, email, password })
+    )
+    .then((r) => ({ id: r.insertedId, firstName, lastName, email, password }));
+}
+
+module.exports = { createNewUser, getAllUsers };
