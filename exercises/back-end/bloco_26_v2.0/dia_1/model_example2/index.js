fca8e8ab0b05b8459e13b74bf02a4759ada42509
rodrigoruan <rodrigopython16@gmail.com>
2021-10-08 10:34:12 -0300

    Added createBook

--- exercises/back-end/bloco_26_v2.0/dia_1/model_example2/index.js
+++ exercises/back-end/bloco_26_v2.0/dia_1/model_example2/index.js
@@ -4,6 +4,8 @@ const Books = require('./model/Books');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/books/author/:id', async (req, res) => {
   const { id } = req.params;
   const books = await Books.getByAuthorId(Number(id));
@@ -18,6 +20,18 @@ app.get('/books/:id', async (req, res) => {
   if (!book.length) return res.status(400).json({ message: 'book not found' });
 
   res.status(200).json(book);
+});
+
+app.post('/books', async (req, res) => {
+  const { title, author_id } = req.body;
+
+  const response = await Books.createNewBook(title, author_id);
+
+  if (response === 'Dados inválidos') {
+    return res.status(400).json({ message: 'dados invalidos' });
+  }
+
+  res.status(200).json({ message: 'Livro criado com sucesso' });
 });
 
 app.get('/authors/:id', async (req, res) => {
--- exercises/back-end/bloco_26_v2.0/dia_1/model_example2/model/Books.js
+++ exercises/back-end/bloco_26_v2.0/dia_1/model_example2/model/Books.js
@@ -1,8 +1,37 @@
 const connection = require('./connections');
 
+const validate = (title, author_id, authorExists) => {
+  if (!title || title.length < 3) return false;
+  if (!author_id || !authorExists) return false;
+
+  return true;
+};
+
+const createNewBook = async (title, author_id) => {
+  const [authors] = await connection.execute(
+    'SELECT * FROM authors WHERE id=?',
+    [author_id]
+  );
+
+  const authorExists = authors.some(
+    (author) => author.id === Number(author_id)
+  );
+
+  if (!validate(title, author_id, authorExists)) {
+    return 'Dados inválidos';
+  }
+
+  connection.execute(
+    'INSERT INTO model_example.books (title, author_id) VALUES (? ,?)',
+    [title, author_id]
+  );
+
+  return 'Livro criado com sucesso';
+};
+
 const getByAuthorId = async (q) => {
   const [books] = await connection.execute(
-    `SELECT * FROM authors WHERE author_id = ${q}`
+    `SELECT * FROM books WHERE author_id = ${q}`
   );
 
   return books;
@@ -30,4 +59,10 @@ const getAllBooks = async () => {
   return books;
 };
 
-module.exports = { getAllBooks, getByAuthorId, findAuthorById, findBookById };
+module.exports = {
+  getAllBooks,
+  getByAuthorId,
+  findAuthorById,
+  findBookById,
+  createNewBook,
+};
