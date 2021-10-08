8b667de1340f56aae2b5032d0f6ae7a310cd425d
rodrigoruan <rodrigopython16@gmail.com>
2021-10-08 09:23:45 -0300

    Done

--- exercises/back-end/bloco_26/dia_5/index.js
+++ exercises/back-end/bloco_26/dia_5/index.js
@@ -12,18 +12,18 @@ app.use(bodyParser.json());
 
 // app.use("/user", userRouter);
 
-// app.get("/btc", async (req, res) => {
-//   const token = req.headers.authorization;
+app.get("/btc", async (req, res) => {
+  const token = req.headers.authorization;
 
-//   if (token.length !== 12 || /^[^A-Za-z\d]{12}$/.test(token)) {
-//     return res.status(400).json({ message: "invalid token" });
-//   }
+  if (token.length !== 12 || /^[^A-Za-z\d]{12}$/.test(token)) {
+    return res.status(400).json({ message: "invalid token" });
+  }
 
-//   const json = await axios.get(
-//     "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
-//   );
-//   res.status(200).json(json.data);
-// });
+  const json = await axios.get(
+    "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+  );
+  res.status(200).json(json.data);
+});
 
 // const posts = [{ id: 1, name: "HELLO" }];
 
@@ -51,30 +51,30 @@ app.use(bodyParser.json());
 //   res.status(404).json({ message: "Opsss, route not found" });
 // });
 
-app.post("/teams", (req, res) => {
-  const { name, initials, country, league } = req.body;
+// app.post("/teams", (req, res) => {
+//   const { name, initials, country, league } = req.body;
 
-  if (name < 5 || !/[A-Z]{1,3}/.test(initials) || country.length < 3) {
-    return res.status(400).json({ message: "invalid data" });
-  }
+//   if (name < 5 || !/[A-Z]{1,3}/.test(initials) || country.length < 3) {
+//     return res.status(400).json({ message: "invalid data" });
+//   }
 
-  const infos = JSON.stringify({ name, initials, country, league });
+//   const infos = JSON.stringify({ name, initials, country, league });
 
-  fs.writeFileSync(
-    `/home/rodrigo/Documents/trybe-exercises/exercises/back-end/bloco_26/dia_5/teams.json`,
-    infos
-  );
+//   fs.writeFileSync(
+//     `/home/rodrigo/Documents/trybe-exercises/exercises/back-end/bloco_26/dia_5/teams.json`,
+//     infos
+//   );
 
-  res.status(200).json(infos);
-});
+//   res.status(200).json(infos);
+// });
 
-app.get("/teams", (_req, res) => {
-  const file = fs.readFileSync("./teams.json", "utf-8");
+// app.get("/teams", (_req, res) => {
+//   const file = fs.readFileSync("./teams.json", "utf-8");
 
-  if (!file) return res.status(200).json({ teams: [] });
+//   if (!file) return res.status(200).json({ teams: [] });
 
-  res.status(200).json(JSON.parse(file));
-});
+//   res.status(200).json(JSON.parse(file));
+// });
 
 app.listen(3001, () => {
   console.log("Ouvindo na porta 3001");
