8224d141f775b3cfe671ed6548c0e94655d2a16b
rodrigoruan <rodrigopython16@gmail.com>
2021-10-11 10:31:03 -0300

    Added exercise 4 and 5

--- exercises/back-end/bloco_26_v2.0/dia_2/cep_lookup/index.js
+++ exercises/back-end/bloco_26_v2.0/dia_2/cep_lookup/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/ping', async (_req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
 app.listen(PORT, () => {
   console.log(`Ouvindo na porta: ${PORT}`);
 });
