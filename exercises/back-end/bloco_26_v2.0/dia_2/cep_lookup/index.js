05015f855ecb477602eabb70d6646c71caea5c74
rodrigoruan <rodrigopython16@gmail.com>
2021-10-11 10:50:05 -0300

    Added message to invalid data

--- exercises/back-end/bloco_26_v2.0/dia_2/cep_lookup/index.js
+++ exercises/back-end/bloco_26_v2.0/dia_2/cep_lookup/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const CepFunctions = require('./models/Cep');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -8,8 +10,20 @@ app.get('/ping', async (_req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
+app.post('/')
+
 app.get('/cep/:cep', async (req, res) => {
   const { cep } = req.params;
+
+  const response = await CepFunctions.findCep(cep);
+
+  if (!response) {
+    return res
+      .status(400)
+      .json({ error: { code: 'invalidData', message: 'CEP inválido' } });
+  }
+
+  res.status(200).json(response);
 });
 
 app.listen(PORT, () => {
--- exercises/back-end/bloco_26_v2.0/dia_2/cep_lookup/models/Cep.js
+++ exercises/back-end/bloco_26_v2.0/dia_2/cep_lookup/models/Cep.js
@@ -1,1 +1,25 @@
 const connection = require('./connection');
+
+const createNewCep = async (cep, log, bairro, local, uf) => {
+  const res = connection.execute(
+    'INSERT INTO ceps (cep, logradouro, bairro, localidade, uf) VALUES (?, ?, ?, ?, ?)',
+    [cep, log, bairro, local, uf]
+  );
+
+  return res;
+};
+
+const findCep = async (cep) => {
+  const [returnedCep] = await connection.execute(
+    'SELECT * FROM ceps WHERE cep = ?',
+    [cep]
+  );
+
+  if (!returnedCep.length) return false;
+
+  return returnedCep;
+};
+
+module.exports = {
+  findCep,
+};
