e1122425ee624db5bfde0f93f4012be1e6d6ff89
rodrigoruan <rodrigopython16@gmail.com>
2021-10-13 11:18:43 -0300

    Created database and productModel

---
+++ exercises/back-end/bloco_26_v2.0/dia_3/exercises/models/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'senha123',
+  database: 'rest_exercicios',
+});
+
+module.exports = {
+  connection,
+};
