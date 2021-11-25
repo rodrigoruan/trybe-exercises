352305c885c5034622f1ffe55d5328bbdcda69c1
Rodrigo <rodrigopython16@gmail.com>
2021-11-25 14:11:29 -0300

    done examples

---
+++ exercises/back-end/bloco_29/dia_2/seq-test/seeders/20211125163602-users.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          fullName: 'Leonardo',
+          email: 'leo@test.com',
+          createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+          updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        {
+          fullName: 'JEduardo',
+          email: 'edu@test.com',
+          createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+          updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+      ],
+      {}
+    ),
+
+  down: async (queryInterface, _Sequelize) =>
+    queryInterface.bulkDelete('Users', null, {}),
+};
