bfe3f9b3e8e2db8bab43dbe069328d78938cd260
Rodrigo <rodrigopython16@gmail.com>
2022-01-17 12:11:18 -0300

    ex 14

--- exercises/computer-science/bloco_32/dia_1/loops.py
+++ exercises/computer-science/bloco_32/dia_1/loops.py
@@ -1,8 +1,17 @@
-n = 5
+# n = 5
 
-total = 1
+# total = 1
 
-for i in range(n): 
-  total *= (i + 1)
+# for i in range(n): 
+#   total *= (i + 1)
 
-print(total)
+# print(total)
+
+ratings = [6, 8, 5, 9, 10]
+
+new_ratings = []
+
+for rating in ratings:
+  new_ratings.append(rating * 10)
+
+print(new_ratings)
