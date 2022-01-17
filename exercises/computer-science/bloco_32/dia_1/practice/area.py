33ae66611e26add04ed6410b10f6f8e3ec25b026
Rodrigo <rodrigopython16@gmail.com>
2022-01-17 13:03:10 -0300

    pushing exercises

---
+++ exercises/computer-science/bloco_32/dia_1/practice/area.py
@@ -0,0 +1,15 @@
+PI = 3.14
+
+
+def square(side):
+    return side * side
+
+
+def rectangle(length, width):
+    return length * width
+
+
+def circle(radius):
+    return PI * radius * radius
+
+print(square(5))
