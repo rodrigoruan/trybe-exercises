4e5bd261be1c389958fac1946488bc596be225cb
Rodrigo <rodrigopython16@gmail.com>
2022-01-22 11:31:01 -0300

    feature: filter only reproved students

---
+++ exercises/computer-science/bloco_32/dia_2/ex_praticos/ex3.py
@@ -0,0 +1,13 @@
+with open("students.txt", mode="r") as file:
+  students = file.read().split('\n')
+
+  reprovedStudents = list()
+  
+  for student in students:
+    student, score = student.split(' ')
+    if int(score) < 6:
+      reprovedStudents.append((f"{student} {score}"))
+  
+  print(reprovedStudents)
+
+  file.close()
