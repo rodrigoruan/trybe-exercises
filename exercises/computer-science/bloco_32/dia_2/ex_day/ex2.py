472959c6b05b1116431a2edc2ca616fa6a0bd1ef
Rodrigo <rodrigopython16@gmail.com>
2022-01-22 12:22:30 -0300

    refactor: reading words from ex2.txt

--- exercises/computer-science/bloco_32/dia_2/ex_day/ex2.py
+++ exercises/computer-science/bloco_32/dia_2/ex_day/ex2.py
@@ -1,17 +1,20 @@
 import random
 
-list_of_words = ["paralelepipedo", "morango", "cachorro", "escudo"]
-index = random.randint(0, len(list_of_words) - 1)
+with open("./ex2.txt", "r") as file:
+  list_of_words = file.read().split('\n')
 
-selected_word = list_of_words[index]
-scrambled_word = "".join(random.sample(selected_word, len(selected_word)))
+  index = random.randint(0, len(list_of_words) - 1)
 
-print(f"Try to guess the word: {scrambled_word}")
+  selected_word = list_of_words[index]
+  scrambled_word = "".join(random.sample(selected_word, len(selected_word)))
 
-for i in range(3):
-  guess = input("type here: ")
-  if guess == selected_word:
-    print("Congratulations!!! you won!")
-    break
-  else:
-    print(f"Wrong guess, try more {2 - i} times")
+  print(f"Try to guess the word: {scrambled_word}")
+
+  for i in range(3):
+    guess = input("type here: ")
+    if guess == selected_word:
+      print("Congratulations!!! you won!")
+      break
+    else:
+      print(f"Wrong guess, try more {2 - i} times")
+
